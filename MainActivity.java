8666236e24a078b2a509ad69b1700d98b721e2df
evenvela <58957148+evenvela@users.noreply.github.com>
2020-02-22 00:59:45 -0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -6,24 +6,70 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.answer1).setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                ((TextView)findViewById(R.id.flashcard_question)).setVisibility(View.INVISIBLE);
-                //make answer appear
-                ((TextView)findViewById(R.id.answer)).setVisibility(View.VISIBLE);
+            public void onClick(View v)
+            {
+                if(((TextView)findViewById(R.id.flashcard_question)).getTag().equals("1"))
+                    ((TextView) findViewById(R.id.answer1)).setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                else
+                    ((TextView) findViewById(R.id.answer1)).setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+                if(((TextView)findViewById(R.id.flashcard_question)).getTag().equals("2"))
+                    ((TextView) findViewById(R.id.answer2)).setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+                if(((TextView)findViewById(R.id.flashcard_question)).getTag().equals("3"))
+                    ((TextView) findViewById(R.id.answer3)).setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
             }
         });
 
 
-    }
+        findViewById(R.id.answer2).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if(((TextView)findViewById(R.id.flashcard_question)).getTag().equals("1"))
+                    ((TextView) findViewById(R.id.answer1)).setBackgroundColor(getResources().getColor(R.color.colorPrimary));
 
+                if(((TextView)findViewById(R.id.flashcard_question)).getTag().equals("2"))
+                    ((TextView) findViewById(R.id.answer2)).setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                else
+                    ((TextView) findViewById(R.id.answer2)).setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+                if(((TextView)findViewById(R.id.flashcard_question)).getTag().equals("3"))
+                    ((TextView) findViewById(R.id.answer3)).setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+            }
+        });
+        findViewById(R.id.answer3).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if(((TextView)findViewById(R.id.flashcard_question)).getTag().equals("1"))
+                    ((TextView) findViewById(R.id.answer1)).setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+                if(((TextView)findViewById(R.id.flashcard_question)).getTag().equals("2"))
+                    ((TextView) findViewById(R.id.answer2)).setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+                if(((TextView)findViewById(R.id.flashcard_question)).getTag().equals("3"))
+                    ((TextView) findViewById(R.id.answer3)).setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                else
+                    ((TextView) findViewById(R.id.answer3)).setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+            }
+        });
+    }
 
 }
